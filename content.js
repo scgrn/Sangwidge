75385f8f8cec8521d3f6ca7e59ab019ae8225c97
Andrew Krause <ajkrause@gmail.com>
2023-05-04 21:17:12 -0400

    Update to manifest V3 and handle plural form

---
+++ content.js
@@ -0,0 +1,40 @@
+function matchCase(text, pattern) {
+    var result = '';
+
+    for (var i = 0; i < text.length; i++) {
+        var c = text.charAt(i);
+        var p = pattern.charCodeAt(i);
+
+        if (p >= 65 && p < 65 + 26) {
+            result += c.toUpperCase();
+        } else {
+            result += c.toLowerCase();
+        }
+    }
+
+    return result;
+}
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+        
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/sandwiches/gi, function(match) {
+                return matchCase("sangwidges", match);
+            });
+            replacedText = replacedText.replace(/sandwich/gi, function(match) {
+                return matchCase("sangwidge", match);
+            });
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
